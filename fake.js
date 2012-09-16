0467316b1d1e6b1bcf22224367a304fceec55023
hax <hax@heshijun.net>
2012-09-16 17:01:24 +0800

    update

--- fake.js
+++ fake.js
@@ -6,8 +6,8 @@ var User_Count = 200
 for (var i = 0; i < User_Count; i++) {
 	users[i] = {
 		userId: lt.randomString(4) + '@exmaple.com',
-		userNumber: lt.randomInteger(10000)
-		salt: lt.randomString(4),
+		userNumber: lt.randomInteger(10000),
+		salt: lt.randomString(4)
 	}
 }
 
